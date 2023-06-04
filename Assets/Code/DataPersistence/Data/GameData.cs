1d17c6bdb0c43ee250816b3ebd70502ae8b19d19
Vu Linh Duc (K15 HL) <@ducvlhe153739>
2023-06-04 14:13:43 +0700

    cannon turret

---
+++ Assets/Code/DataPersistence/Data/GameData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int roundCount;
+
+    public GameData()
+    {
+        this.roundCount = 0;
+    }
+}
